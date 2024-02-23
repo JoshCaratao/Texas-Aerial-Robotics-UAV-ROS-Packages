b01bc13be875d541f3952802feae2eaefd441d5c
Joshua Caratao <53455636+JoshCaratao@users.noreply.github.com>
2024-02-22 18:38:11 -0600

    Add files via upload

--- controls/include/controls/PID.h
+++ controls/include/controls/PID.h
@@ -1,1 +1,23 @@
 
+#ifndef PID_H
+#define PID_H
+
+class PID {
+    
+    public:
+        PID(double p, double i, double d); //Constructor
+        double calculate(double error, double time);
+
+    private:
+
+        double P;
+        double I;
+        double D;
+
+        double totalPID;
+        double lastError;
+        double lastTime;
+
+};
+
+#endif
